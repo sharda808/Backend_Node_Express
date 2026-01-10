f45bb0b373fe3f726c466f590983e6cbcc6a5065
sharda <shardakumari96115@gmail.com>
2026-01-10 19:18:30 +0530

    Node-1

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 console.log('I was here');
  const requestHandle=(req,res) => {
-console.log('I was here in handler');
+console.log('I was here in handler',req);
 }
 const server = http.createServer(requestHandle);
-server.listen(3000);
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}`)
+})
