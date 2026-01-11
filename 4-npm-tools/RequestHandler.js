a4d2feef717c08af5d3efcb1f8f3116a335f05bd
sharda <shardakumari96115@gmail.com>
2026-01-11 22:24:35 +0530

    npm-tools

---
+++ 4-npm-tools/RequestHandler.js
@@ -0,0 +1,81 @@
+ const fs = require('fs');
+const {URLSearchParams} = require('url');const RequestHandler=(req,res) => {
+console.log('Request Received', req.url, req.method);
+res.setHeader('Content-Type', 'text/html');
+let arr = [1,3,5,7,9];
+for(let i=0; i<arr.length; i++){
+  console.log(arr[i]);
+}
+if(req.url === "/"){
+res.write(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <title>Document</title>
+</head>
+<body>
+  <h1>Welcome to First server</h1>
+  <form action ="/buy-product" method="POST">
+<input type="text" placeholder="Enter the product that you want" name="product">
+<br />
+<input type="text" placeholder="Enter your budget" name="budget">
+<input type="submit">
+  </form>
+</body>
+</html>`);
+res.end();
+}
+else if(req.url === "/buy-product"){
+  console.log("Form data received");
+  const buffer = [];
+  req.on('data',(chunk) => {
+    console.log(chunk);
+    buffer.push(chunk);
+  });
+  req.on('end',()=> {
+    const body = Buffer.concat(buffer).toString();
+    const urlParams =  new URLSearchParams(body);
+    const bodyJson = {};
+    for(const[key,value] of urlParams.entries()){
+bodyJson[key] = value;
+    }
+    console.log(JSON.stringify(bodyJson));
+  fs.writeFile('buy.txt', JSON.stringify(bodyJson),(err) => {
+  res.statusCode = 302;
+  res.setHeader ('Location', '/product');
+  res.end();
+  console.log('Sending Resposne');
+  })
+  });
+
+}
+else if(req.url === "/product"){
+res.write(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <title>Products</title>
+</head>
+<body>
+  <h1>Product list will appear here.</h1>
+</body>
+</html>`);
+
+}
+else{
+  res.statusCode = 404;
+  res.write(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <title>Error</title>
+</head>
+<body>
+  <h1>404 Page not found</h1>
+</body>
+</html>`)
+}
+
+ }
+
+ exports.handler = RequestHandler;
