19f88fe2b6c60978740322d5dd6078d4a52b55aa
sharda <shardakumari96115@gmail.com>
2026-01-13 23:24:02 +0530

    dynamic-path

---
+++ 9-dynamic-path/routers/hostRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const hostRouter = express.Router();
+const hostController = require('./../controllers/hostController')
+hostRouter.get("/add-home",hostController.getAddHome);
+hostRouter.post("/add-home",hostController.postAddHome)
+exports.hostRouter = hostRouter;
+
